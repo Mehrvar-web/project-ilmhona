d8dd6ba944e8d974067af8674e1b40690ed9b7fe
Mehrvar-web <mehrvarshodiev2406@mail.ru>
2023-01-17 00:39:43 +0500

    popup added

--- js/app.js
+++ js/app.js
@@ -25,3 +25,43 @@ document.onclick = function(e) {
         document.querySelector(".logo").style.position = "absolute";
     }
 };
+
+// Contact Section
+const popup = document.querySelector(".popup");
+const subscribeBtn = document.querySelector("#subscribe-btn");
+const emailInput = document.querySelector(".email-input");
+const emailError = document.querySelector("#email-error");
+const pattern = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,5}$/;
+
+// Show Popup by clicking subscribe btn
+function openPopup() {
+    if (emailInput.value.match(pattern)) {
+        popup.classList.add("open-popup");
+        emailInput.value = "";
+    } else {
+        emailError.innerHTML = "Please enter a valid email";
+    }
+}
+// Close Popup by clicking OK btn
+function closePopup() {
+    popup.classList.remove("open-popup");
+}
+
+// Check validation of Email
+function validateEmail() {
+
+    if (!emailInput.value.match(pattern)) {
+        emailInput.style.outlineColor = "red";
+        emailError.innerHTML = "Please enter a valid email";
+        emailError.style.color = "red";
+        emailError.style.bottom = "-27px";
+        return false;
+    } else {
+        emailError.innerHTML = "";
+        emailInput.style.outlineColor = "green";
+        emailError.innerHTML = "";
+        emailError.style.bottom = "-20px";
+        return true;
+    }
+
+}
